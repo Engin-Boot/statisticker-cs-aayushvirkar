83f42516d8ecb00918c1bf52479066ad64136dd4
aayushvirkar <51874905+aayushvirkar@users.noreply.github.com>
2020-09-04 00:59:22 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 
 namespace Statistics
 {
+    public class Metrics
+    {
+        public float average;
+        public float max;
+        public float min;
+    }
     public class StatsComputer
     {
         public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+            
+           Metrics stats = new Metrics();
+            
+            if(numbers.Count==0_
+               {
+                   stats.average = float.NaN;
+                   stats.max = float.NaN;
+                   stats.min = float.NaN;
+               }
+              else 
+               {
+                 stats.average = numbers.Average();
+                 stats.max = numbers.Max();
+                 stats.min = numbers.Min();
+               }
+          return stats;
         }
     }
 }
