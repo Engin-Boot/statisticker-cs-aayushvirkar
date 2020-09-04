d2f60d2544e8675496c4b2592d245b15e1d0f30b
aayushvirkar <51874905+aayushvirkar@users.noreply.github.com>
2020-09-04 13:01:28 +0530

    Update StatsUnitTest.cs

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -14,9 +14,9 @@ namespace Statistics.Test
             var computedStats = statsComputer.CalculateStatistics(
                 new List<float>{1.5, 8.9, 3.2, 4.5});
             float epsilon = 0.001F;
-            Assert.True(Math.Abs(statsComputer.average - 4.525) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.max - 8.9) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.min - 1.5) <= epsilon);
+            Assert.True(Math.Abs(computedStats.average - 4.525) <= epsilon);
+            Assert.True(Math.Abs(computedStats.max - 8.9) <= epsilon);
+            Assert.True(Math.Abs(computedStats.min - 1.5) <= epsilon);
         }
         [Fact]
         public void ReportsNaNForEmptyInput()
@@ -24,9 +24,9 @@ namespace Statistics.Test
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
                 new List<float>{});
-            Assert.True(Math.abs(statsComputer.average.Equals(float.NaN)));
-            Assert.True(Math.abs(statsComputer.min.Equals(float.NaN)));
-            Assert.True(Math.abs(statsComputer.max.Equals(float.NaN)));
+            Assert.True(computedStats.average.Equals(float.NaN));
+            Assert.True(computedStats.min.Equals(float.NaN));
+            Assert.True(computedStats.max.Equals(float.NaN));
                        
         }
     }
